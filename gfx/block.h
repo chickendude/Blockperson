33542388e02cbaa495a610d562e427f70f9b5c91
Michael Nock <michael.nock@ifit.com>
2021-09-02 14:41:34 -0400

    Add block drawing routine

---
+++ gfx/block.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(block)
+
+//======================================================================
+//
+//	block, 64x16@4, 
+//	+ palette 256 entries, not compressed
+//	+ 16 tiles Metatiled by 2x2 not compressed
+//	Total size: 512 + 512 = 1024
+//
+//	Time-stamp: 2021-09-02, 14:40:50
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_BLOCK_H
+#define GRIT_BLOCK_H
+
+#define blockTilesLen 512
+extern const unsigned int blockTiles[128];
+
+#define blockPalLen 512
+extern const unsigned short blockPal[256];
+
+#endif // GRIT_BLOCK_H
+
+//}}BLOCK(block)
---
+++ gfx/blockperson.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(blockperson)
+
+//======================================================================
+//
+//	blockperson, 160x16@4, 
+//	+ palette 256 entries, not compressed
+//	+ 40 tiles Metatiled by 2x2 not compressed
+//	Total size: 512 + 1280 = 1792
+//
+//	Time-stamp: 2021-09-02, 14:33:54
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.16
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_BLOCKPERSON_H
+#define GRIT_BLOCKPERSON_H
+
+#define blockpersonTilesLen 1280
+extern const unsigned int blockpersonTiles[320];
+
+#define blockpersonPalLen 512
+extern const unsigned short blockpersonPal[256];
+
+#endif // GRIT_BLOCKPERSON_H
+
+//}}BLOCK(blockperson)
